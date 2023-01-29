6657531ef05e7b10f6f95f10c6d08b6e11da1d6f
xantinium <chapliyva@gmail.com>
2023-01-29 21:06:42 +0300

    chakra init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import {AppService} from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello')
-  getHello(): string {
+  @Get('methods')
+  getHello() {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import {Injectable} from '@nestjs/common';
 
+type MethodType = {
+  id: string;
+  title: string;
+  desc: string;
+};
+
+const methods: MethodType[] = [
+  {
+      id: '1',
+      title: 'Метод 1',
+      desc: 'Это супер-пупер метод, наверное'
+  }
+];
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return '<h1>Я тебя люблю ❤️</h1>';
+  getHello() {
+    return methods;
   }
 }
