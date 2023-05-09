b90b95d80be3b6806411f2e195b6644c5e9c3bcc
dashka <misheninadar@yandex.ru>
2023-05-09 19:19:43 +0300

    skdn

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,18 @@ import {Injectable} from '@nestjs/common';
 import {MethodType, MethodItemType} from './types';
 import * as method_1 from './data/1.json';
 import * as method_2 from './data/2.json';
+import * as method_3 from './data/3.json';
+import * as method_4 from './data/4.json';
+import * as method_5 from './data/5.json';
+import * as method_6 from './data/6.json';
 
 const METHODS: MethodType[] = [
   method_1,
   method_2,
+  method_3,
+  method_4,
+  method_5,
+  method_6,
 ];
 
 @Injectable()
