da30288cf9338f9b49dfa7d1ae5def97db155c36
dashka <misheninadar@yandex.ru>
2023-05-28 10:57:41 +0300

    ,mnb,djv

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,6 +7,8 @@ import * as method_3 from './data/3.json';
 import * as method_4 from './data/4.json';
 import * as method_5 from './data/5.json';
 import * as method_6 from './data/6.json';
+import * as method_7 from './data/7.json';
+import * as method_8 from './data/8.json';
 
 const METHODS: MethodType[] = [
   method_1,
@@ -15,6 +17,8 @@ const METHODS: MethodType[] = [
   method_4,
   method_5,
   method_6,
+  method_7,
+  method_8,
 ];
 
 @Injectable()
