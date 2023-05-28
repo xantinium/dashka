36c02a6e8e879a52a26e2550cba01774d54d7e0e
xantinium <chapliyva@gmail.com>
2023-05-28 10:51:25 +0300

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,6 +18,12 @@ export class AppController {
     return this.appService.getMethod(params.id);
   }
 
+  @Get('img/:id')
+  getImage(@Param() params: {id: string}) {
+    const file = createReadStream(join(process.cwd(), `./src/img/${params.id}.png`));
+    return new StreamableFile(file, {type: 'image/png'});
+  }
+
   @Get('.well-known/pki-validation/1993448A256C5A60A69172FD32253BC7.txt')
   getSSL() {
     const file = createReadStream(join(process.cwd(), './secrets/1993448A256C5A60A69172FD32253BC7.txt'));
